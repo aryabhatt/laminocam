232f69e02dfbe1994b06bc9342818b55eb7ff34e
Dinesh Kumar <dkumar@lbl.gov>
2026-06-29 17:38:56 -0700

    Refactor NUFFT 3D functions to use plan cache and remove direct FINUFFT calls

--- include/nufft.h
+++ include/nufft.h
@@ -21,83 +21,41 @@
 #ifndef NUFFT__H
 #define NUFFT__H
 
-#include <cmath>
+#include <array>
 #include <complex>
-#include <finufft.h>
-#include <vector>
+#include <cstdint>
 
 #include "array.h"
 #include "dtypes.h"
+#include "finufft_plan_cache.h"
 #include "polar_grid.h"
 
 namespace tomocam::nufft {
+
     // 3D Type-1 NUFFT: nonuniform points to uniform grid
     template <typename T>
     void nufft3d1(const Array<std::complex<T>> &cz, Array<std::complex<T>> &fz,
                   const PolarGrid<T> &pg) {
-        int M = pg.npts;
-        int N1 = fz.nslices();
-        int N2 = fz.nrows();
-        int N3 = fz.ncols();
-        finufft_opts *opts = new finufft_opts;
-
-        if (std::is_same_v<T, double>) {
-            finufft_default_opts(opts);
-            opts->upsampfac = 2.0;
-            T tol = 1e-14;
-            double *x = (double *)pg.x.begin();
-            double *y = (double *)pg.y.begin();
-            double *z = (double *)pg.z.begin();
-            std::complex<double> *cptr = (std::complex<double> *)cz.begin();
-            std::complex<double> *fptr = (std::complex<double> *)fz.begin();
-            finufft3d1(M, x, y, z, cptr, 1, tol, N3, N2, N1, fptr, opts);
-        } else {
-            finufftf_default_opts(opts);
-            opts->upsampfac = 2.0;
-            T tol = 1.2e-06;
-            float *x = (float *)pg.x.begin();
-            float *y = (float *)pg.y.begin();
-            float *z = (float *)pg.z.begin();
-            std::complex<float> *cptr = (std::complex<float> *)cz.begin();
-            std::complex<float> *fptr = (std::complex<float> *)fz.begin();
-            finufftf3d1(M, x, y, z, cptr, 1, tol, N3, N2, N1, fptr, opts);
-        }
-        delete opts;
+        std::array<int64_t, 3> n_modes = {static_cast<int64_t>(fz.ncols()),
+                                          static_cast<int64_t>(fz.nrows()),
+                                          static_cast<int64_t>(fz.nslices())};
+        auto &plan = plans::cache<T>.get_plan(1, 3, n_modes, 1);
+        plan.set_points(pg);
+        plan.execute(const_cast<std::complex<T> *>(cz.begin()), fz.begin());
     }
 
     // 3D Type-2 NUFFT: uniform grid to nonuniform points
     template <typename T>
     void nufft3d2(Array<std::complex<T>> &cz, const Array<std::complex<T>> &fz,
                   const PolarGrid<T> &pg) {
-        auto M = pg.npts;
-        auto N1 = fz.nslices();
-        auto N2 = fz.nrows();
-        auto N3 = fz.ncols();
-        finufft_opts *opts = new finufft_opts;
-
-        if (std::is_same_v<T, double>) {
-            finufft_default_opts(opts);
-            opts->upsampfac = 2.0;
-            T tol = 1e-14;
-            double *x = (double *)pg.x.begin();
-            double *y = (double *)pg.y.begin();
-            double *z = (double *)pg.z.begin();
-            std::complex<double> *cptr = (std::complex<double> *)cz.begin();
-            std::complex<double> *fptr = (std::complex<double> *)fz.begin();
-            finufft3d2(M, x, y, z, cptr, -1, tol, N3, N2, N1, fptr, opts);
-        } else {
-            finufftf_default_opts(opts);
-            opts->upsampfac = 2.0;
-            T tol = 1.2e-06;
-            float *x = (float *)pg.x.begin();
-            float *y = (float *)pg.y.begin();
-            float *z = (float *)pg.z.begin();
-            std::complex<float> *cptr = (std::complex<float> *)cz.begin();
-            std::complex<float> *fptr = (std::complex<float> *)fz.begin();
-            finufftf3d2(M, x, y, z, cptr, -1, tol, N3, N2, N1, fptr, opts);
-        }
-        delete opts;
+        std::array<int64_t, 3> n_modes = {static_cast<int64_t>(fz.ncols()),
+                                          static_cast<int64_t>(fz.nrows()),
+                                          static_cast<int64_t>(fz.nslices())};
+        auto &plan = plans::cache<T>.get_plan(2, 3, n_modes, -1);
+        plan.set_points(pg);
+        plan.execute(cz.begin(), const_cast<std::complex<T> *>(fz.begin()));
     }
+
 } // namespace tomocam::nufft
 
 #endif // NUFFT__H
