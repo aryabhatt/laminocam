262d7909a4c9b9c594e5f9046d5b9e90d8338d3a
Dinesh Kumar <dkumar@lbl.gov>
2026-06-29 17:37:32 -0700

    Refactor FFT functions to use plan cache and remove direct FFTW calls

--- include/fft.h
+++ include/fft.h
@@ -18,323 +18,119 @@
  *---------------------------------------------------------------------------------
  */
 
+#ifndef FFTDEFS__H
+#define FFTDEFS__H
+
+#include <array>
 #include <complex>
-#include <fftw3.h>
-#include <new>
-#include <type_traits>
 
 #include "array.h"
 #include "dtypes.h"
-
-#ifndef FFTDEFS__H
-    #define FFTDEFS__H
+#include "fftw_plan_cache.h"
 
 namespace tomocam::fft {
 
     template <typename T>
     Array<std::complex<T>> fft1(const Array<std::complex<T>> &input) {
-
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
-        // create return array
+        using complex_t = std::complex<T>;
         dims_t dims = input.dims();
-        Array<std::complex<T>> output(dims);
+        Array<complex_t> output(dims);
 
-        int rank = 1;
-        int n[] = {static_cast<int>(dims.z())};
-        int idist = static_cast<int>(dims.z());
-        int odist = static_cast<int>(dims.z());
-        int istride = 1;
-        int ostride = 1;
-        int *inembed = NULL;
-        int *onembed = NULL;
-        int batches = static_cast<int>(dims.x() * dims.y());
-
-        if (is_double) {
-            auto *idata = (fftw_complex *)input.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)input.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        int howmany = static_cast<int>(dims.x() * dims.y());
+        std::array<int, 3> n = {static_cast<int>(dims.z()), 0, 0};
+        auto &plan = plans::cache<T>.get_plan(1, n, howmany, FftwType::C2C_FORWARD);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
-    Array<std::complex<T>> ifft1(const Array<std::complex<T>> &arr) {
+    Array<std::complex<T>> ifft1(const Array<std::complex<T>> &input) {
+        using complex_t = std::complex<T>;
+        dims_t dims = input.dims();
+        Array<complex_t> output(dims);
 
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
-        // create return array
-        dims_t dims = arr.dims();
-        Array<std::complex<T>> output(arr.dims());
-
-        int rank = 1;
-        int n[] = {static_cast<int>(dims.z())};
-        int idist = static_cast<int>(dims.z());
-        int odist = static_cast<int>(dims.z());
-        int istride = 1;
-        int ostride = 1;
-        int *inembed = NULL;
-        int *onembed = NULL;
-        int batches = static_cast<int>(dims.x() * dims.y());
-
-        if (is_double) {
-            auto *idata = (fftw_complex *)arr.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)arr.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        int howmany = static_cast<int>(dims.x() * dims.y());
+        std::array<int, 3> n = {static_cast<int>(dims.z()), 0, 0};
+        auto &plan = plans::cache<T>.get_plan(1, n, howmany, FftwType::C2C_BACKWARD);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
     Array<std::complex<T>> fft2(const Array<std::complex<T>> &input) {
-
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
-        // create return array
+        using complex_t = std::complex<T>;
         dims_t dims = input.dims();
-        Array<std::complex<T>> output(input.dims());
+        Array<complex_t> output(dims);
 
-        int rank = 2;
-        int n[] = {static_cast<int>(dims.y()), static_cast<int>(dims.z())};
-        int idist = static_cast<int>(dims.y() * dims.z());
-        int odist = static_cast<int>(dims.y() * dims.z());
-        int istride = 1;
-        int ostride = 1;
-        int *inembed = NULL;
-        int *onembed = NULL;
-        int batches = static_cast<int>(dims.x());
-
-        if (is_double) {
-            auto *idata = (fftw_complex *)input.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)input.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_FORWARD, FFTW_ESTIMATE);
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        int howmany = static_cast<int>(dims.x());
+        std::array<int, 3> n = {static_cast<int>(dims.y()), static_cast<int>(dims.z()), 0};
+        auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::C2C_FORWARD);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
     Array<std::complex<T>> ifft2(const Array<std::complex<T>> &input) {
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
-        // create return array
+        using complex_t = std::complex<T>;
         dims_t dims = input.dims();
-        Array<std::complex<T>> output(input.dims());
+        Array<complex_t> output(dims);
 
-        int rank = 2;
-        int n[] = {static_cast<int>(dims.y()), static_cast<int>(dims.z())};
-        int idist = static_cast<int>(dims.y() * dims.z());
-        int odist = static_cast<int>(dims.y() * dims.z());
-        int istride = 1;
-        int ostride = 1;
-        int *inembed = NULL;
-        int *onembed = NULL;
-        int batches = static_cast<int>(dims.x());
-
-        if (is_double) {
-            auto *idata = (fftw_complex *)input.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)input.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_many_dft(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_BACKWARD, FFTW_ESTIMATE);
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        int howmany = static_cast<int>(dims.x());
+        std::array<int, 3> n = {static_cast<int>(dims.y()), static_cast<int>(dims.z()), 0};
+        auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::C2C_BACKWARD);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
     Array<std::complex<T>> fft2_r2c(const Array<T> &input) {
-
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
+        using complex_t = std::complex<T>;
         dims_t dims = input.dims();
-        // create return array
         dims_t out_dims = {dims.n1, dims.n2, dims.n3 / 2 + 1};
+        Array<complex_t> output(out_dims);
 
-        Array<std::complex<T>> output(out_dims);
-
-        int rank = 2;
-        int n[] = {static_cast<int>(dims.n2), static_cast<int>(dims.n3)};
-        int idist = static_cast<int>(dims.n2 * dims.n3);
-        int odist = static_cast<int>(out_dims.n2 * out_dims.n3);
-        int istride = 1;
-        int ostride = 1;
-        int *inembed = nullptr;
-        int *onembed = nullptr;
-        int batches = static_cast<int>(dims.n1);
-
-        if (is_double) {
-            auto *idata = (double *)input.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_many_dft_r2c(rank, n, batches, idata, inembed,
-                                                    istride, idist, odata, onembed,
-                                                    ostride, odist, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (float *)input.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_many_dft_r2c(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        int howmany = static_cast<int>(dims.n1);
+        std::array<int, 3> n = {static_cast<int>(dims.n2), static_cast<int>(dims.n3), 0};
+        auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::R2C);
+        plan.execute(const_cast<T *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
     Array<T> fft2_c2r(const Array<std::complex<T>> &input, dims_t out_dims) {
-
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
-        dims_t dims = input.dims();
-        // create return array
+        using complex_t = std::complex<T>;
         Array<T> output(out_dims);
 
-        int rank = 2;
-        int n[] = {static_cast<int>(out_dims.n2), static_cast<int>(out_dims.n3)};
-        int idist = static_cast<int>(dims.n2 * dims.n3);
-        int odist = static_cast<int>(out_dims.n2 * out_dims.n3);
-        int istride = 1;
-        int ostride = 1;
-        int *inembed = nullptr;
-        int *onembed = nullptr;
-        int batches = static_cast<int>(dims.n1);
-
-        if (is_double) {
-            auto *idata = (fftw_complex *)input.begin();
-            auto *odata = (double *)output.begin();
-            fftw_plan plan = fftw_plan_many_dft_c2r(rank, n, batches, idata, inembed,
-                                                    istride, idist, odata, onembed,
-                                                    ostride, odist, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)input.begin();
-            auto *odata = (float *)output.begin();
-            fftwf_plan plan = fftwf_plan_many_dft_c2r(
-                rank, n, batches, idata, inembed, istride, idist, odata, onembed,
-                ostride, odist, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        int howmany = static_cast<int>(out_dims.n1);
+        std::array<int, 3> n = {static_cast<int>(out_dims.n2), static_cast<int>(out_dims.n3), 0};
+        auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::C2R);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
     Array<std::complex<T>> fft3(const Array<std::complex<T>> &input) {
-
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
-        // create return array
+        using complex_t = std::complex<T>;
         dims_t dims = input.dims();
-        Array<std::complex<T>> output(input.dims());
+        Array<complex_t> output(dims);
 
-        if (is_double) {
-            auto *idata = (fftw_complex *)input.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_dft_3d(dims.x(), dims.y(), dims.z(), idata,
-                                              odata, -1, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)input.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_dft_3d(dims.x(), dims.y(), dims.z(), idata,
-                                                odata, -1, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
+        std::array<int, 3> n = {static_cast<int>(dims.x()), static_cast<int>(dims.y()),
+                                 static_cast<int>(dims.z())};
+        auto &plan = plans::cache<T>.get_plan(3, n, 1, FftwType::C2C_FORWARD);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
     template <typename T>
     Array<std::complex<T>> ifft3(const Array<std::complex<T>> &input) {
-
-        // double or float
-        bool is_double = std::is_same_v<T, double>;
-
+        using complex_t = std::complex<T>;
         dims_t dims = input.dims();
-        Array<std::complex<T>> output(dims);
+        Array<complex_t> output(dims);
 
-        if (is_double) {
-            auto *idata = (fftw_complex *)input.begin();
-            auto *odata = (fftw_complex *)output.begin();
-            fftw_plan plan = fftw_plan_dft_3d(dims.x(), dims.y(), dims.z(), idata,
-                                              odata, 1, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftw_execute(plan);
-            fftw_destroy_plan(plan);
-        } else {
-            auto *idata = (fftwf_complex *)input.begin();
-            auto *odata = (fftwf_complex *)output.begin();
-            fftwf_plan plan = fftwf_plan_dft_3d(dims.x(), dims.y(), dims.z(), idata,
-                                                odata, 1, FFTW_ESTIMATE);
-            if (!plan) { throw std::bad_alloc(); }
-            fftwf_execute(plan);
-            fftwf_destroy_plan(plan);
-        }
-
+        std::array<int, 3> n = {static_cast<int>(dims.x()), static_cast<int>(dims.y()),
+                                 static_cast<int>(dims.z())};
+        auto &plan = plans::cache<T>.get_plan(3, n, 1, FftwType::C2C_BACKWARD);
+        plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
     }
 
