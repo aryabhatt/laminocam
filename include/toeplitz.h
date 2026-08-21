941e87cc8ed3badf49f039f5b49765ca93265ee2
Dinesh Kumar <dkumar@lbl.gov>
2026-08-21 14:19:40 -0700

    updated Toeplitz kernel calculations based on q-coordinate mask

--- include/toeplitz.h
+++ include/toeplitz.h
@@ -1,3 +1,22 @@
+/* -------------------------------------------------------------------------------
+ * Tomocam Copyright (c) 2018
+ *
+ * The Regents of the University of California, through Lawrence Berkeley
+ * National Laboratory (subject to receipt of any required approvals from the
+ * U.S. Dept. of Energy). All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this software,
+ * please contact Berkeley Lab's Innovation & Partnerships Office at
+ * IPO@lbl.gov.
+ *
+ * NOTICE. This Software was developed under funding from the U.S. Department of
+ * Energy and the U.S. Government consequently retains certain rights. As such,
+ * the U.S. Government has been granted for itself and others acting on its
+ * behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
+ * to reproduce, distribute copies to the public, prepare derivative works, and
+ * perform publicly and display publicly, and to permit other to do so.
+ *---------------------------------------------------------------------------------
+ */
 #ifndef TOEPLITZ_H
 #define TOEPLITZ_H
 
@@ -47,8 +66,8 @@ namespace tomocam::cpu {
                      next_fast_dim(2 * recon_dims.n3 - 1)};
             dims_t fft_dims = {dims_.n1, dims_.n2, dims_.n3 / 2 + 1};
 
-            // unit weights at non-uniform grid points
-            auto ones = Array<complex_t>::ones(proj_dims);
+            // unit weights at non-uniform grid points, |q| <= pi
+            auto ones = array::to_complex<T>(grid.w);
 
             // allocate output array for NUFFT
             Array<complex_t> nufft_out(dims_);
