61e46ad4d4534f27a1cd50f431938def2113c843
Dinesh Kumar <dkumar@lbl.gov>
2026-08-21 13:59:44 -0700

    - Added phase-shift for correctiong center of rotation - Unified interface for using plan cache

--- include/fft.h
+++ include/fft.h
@@ -26,9 +26,52 @@
 
 #include "array.h"
 #include "dtypes.h"
+#include "fft.h"
 #include "fftw_plan_cache.h"
 
 namespace tomocam::fft {
+
+    template <typename T>
+    std::vector<T> fftfreqs(size_t n, T d = 1.0) {
+        std::vector<T> freqs(n);
+        T val = 1.0 / (n * d);
+        size_t N = (n - 1) / 2 + 1;
+        for (size_t i = 0; i < N; ++i) { freqs[i] = static_cast<T>(i) * val; }
+        for (size_t i = N; i < n; ++i) { freqs[i] = static_cast<T>(i - n) * val; }
+        return freqs;
+    }
+
+    template <typename T>
+    std::vector<T> rfftfreqs(size_t n, T d = 1.0) {
+        std::vector<T> freqs(n / 2 + 1);
+        T val = 1.0 / (n * d);
+        size_t N = n / 2 + 1;
+        for (size_t i = 0; i < N; ++i) { freqs[i] = static_cast<T>(i) * val; }
+        return freqs;
+    }
+
+    template <typename T>
+    void phase_shift2d(Array<std::complex<T>> &input,
+                       std::vector<std::array<T, 2>> d) {
+
+        using complex_t = std::complex<T>;
+        dims_t dims = input.dims();
+        T dqx = 2 * M_PI / static_cast<T>(input.dims().n3);
+        T dqy = 2 * M_PI / static_cast<T>(input.dims().n2);
+        for (size_t i = 0; i < dims.n1; ++i) {
+            T dx = d[i][0];
+            T dy = d[i][1];
+            for (size_t j = 0; j < dims.n2; ++j) {
+                for (size_t k = 0; k < dims.n3; ++k) {
+                    T qx = (k + 0.5) * dqx - M_PI;
+                    T qy = (j + 0.5) * dqy - M_PI;
+                    T phase = qx * dx + qy * dy;
+                    complex_t shift = std::exp(-complex_t(0, phase));
+                    input[{i, j, k}] *= shift;
+                }
+            }
+        }
+    }
 
     template <typename T>
     Array<std::complex<T>> fft1(const Array<std::complex<T>> &input) {
@@ -63,7 +106,8 @@ namespace tomocam::fft {
         Array<complex_t> output(dims);
 
         int howmany = static_cast<int>(dims.x());
-        std::array<int, 3> n = {static_cast<int>(dims.y()), static_cast<int>(dims.z()), 0};
+        std::array<int, 3> n = {static_cast<int>(dims.y()),
+                                static_cast<int>(dims.z()), 0};
         auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::C2C_FORWARD);
         plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
@@ -76,7 +120,8 @@ namespace tomocam::fft {
         Array<complex_t> output(dims);
 
         int howmany = static_cast<int>(dims.x());
-        std::array<int, 3> n = {static_cast<int>(dims.y()), static_cast<int>(dims.z()), 0};
+        std::array<int, 3> n = {static_cast<int>(dims.y()),
+                                static_cast<int>(dims.z()), 0};
         auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::C2C_BACKWARD);
         plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
@@ -90,7 +135,8 @@ namespace tomocam::fft {
         Array<complex_t> output(out_dims);
 
         int howmany = static_cast<int>(dims.n1);
-        std::array<int, 3> n = {static_cast<int>(dims.n2), static_cast<int>(dims.n3), 0};
+        std::array<int, 3> n = {static_cast<int>(dims.n2), static_cast<int>(dims.n3),
+                                0};
         auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::R2C);
         plan.execute(const_cast<T *>(input.begin()), output.begin());
         return output;
@@ -102,7 +148,8 @@ namespace tomocam::fft {
         Array<T> output(out_dims);
 
         int howmany = static_cast<int>(out_dims.n1);
-        std::array<int, 3> n = {static_cast<int>(out_dims.n2), static_cast<int>(out_dims.n3), 0};
+        std::array<int, 3> n = {static_cast<int>(out_dims.n2),
+                                static_cast<int>(out_dims.n3), 0};
         auto &plan = plans::cache<T>.get_plan(2, n, howmany, FftwType::C2R);
         plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
@@ -114,8 +161,9 @@ namespace tomocam::fft {
         dims_t dims = input.dims();
         Array<complex_t> output(dims);
 
-        std::array<int, 3> n = {static_cast<int>(dims.x()), static_cast<int>(dims.y()),
-                                 static_cast<int>(dims.z())};
+        std::array<int, 3> n = {static_cast<int>(dims.x()),
+                                static_cast<int>(dims.y()),
+                                static_cast<int>(dims.z())};
         auto &plan = plans::cache<T>.get_plan(3, n, 1, FftwType::C2C_FORWARD);
         plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
@@ -127,8 +175,9 @@ namespace tomocam::fft {
         dims_t dims = input.dims();
         Array<complex_t> output(dims);
 
-        std::array<int, 3> n = {static_cast<int>(dims.x()), static_cast<int>(dims.y()),
-                                 static_cast<int>(dims.z())};
+        std::array<int, 3> n = {static_cast<int>(dims.x()),
+                                static_cast<int>(dims.y()),
+                                static_cast<int>(dims.z())};
         auto &plan = plans::cache<T>.get_plan(3, n, 1, FftwType::C2C_BACKWARD);
         plan.execute(const_cast<complex_t *>(input.begin()), output.begin());
         return output;
